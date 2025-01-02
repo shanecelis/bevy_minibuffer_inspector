9972fd1900664b9cb9e7ad280ffbf5dee5cbc42e
Shane Celis <shane.celis@gmail.com>
2025-01-02 01:36:17 -0500

    chore: Consider renaming it to bevy_minibuffer_inspector_egui.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bevy_minibuffer_inspector/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/bevy_minibuffer_inspector_egui/0.1.1")]
 #![doc = include_str!("../README.md")]
 #![forbid(missing_docs)]
 
